72478460d0bd7e11806bf3efaf36b0f88f812b5d
MatiasGOrtega <matias-gabriel-ortega@outlook.com.ar>
2025-07-13 15:29:06 -0300

    feat: add Hero component with GSAP animations and responsive video handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <main>
       <Navbar />
+      <Hero />
     </main>
   )
 }
