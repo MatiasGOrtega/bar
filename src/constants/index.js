994e8de6a4b8d0884f64f2e2bde2ce51cc737b01
MatiasGOrtega <matias-gabriel-ortega@outlook.com.ar>
2025-07-13 16:11:27 -0300

    feat: add Cocktailds component with GSAP animations and cocktail lists

--- src/constants/index.js
+++ src/constants/index.js
@@ -17,4 +17,58 @@ const navLinks = [
   },
 ];
 
-export { navLinks };
+const cocktailLists = [
+  {
+    name: "Nombre del Cocktail",
+    country: "PAIS",
+    detail: "000 ml",
+    price: "$000",
+  },
+  {
+    name: "Nombre del Cocktail 2",
+    country: "PAIS",
+    detail: "000 ml",
+    price: "$000",
+  },
+  {
+    name: "Nombre del Cocktail 3",
+    country: "PAIS",
+    detail: "000 ml",
+    price: "$000",
+  },
+  {
+    name: "Nombre del Cocktail 4",
+    country: "PAIS",
+    detail: "000 ml",
+    price: "$000",
+  },
+];
+
+const mockTailLists = [
+  {
+    name: "Nombre del Mocktail",
+    country: "PAIS",
+    detail: "000 ml",
+    price: "$000",
+  },
+  {
+    name: "Nombre del Mocktail 2",
+    country: "PAIS",
+    detail: "000 ml",
+    price: "$000",
+  },
+  {
+    name: "Nombre del Mocktail 3",
+    country: "PAIS",
+    detail: "000 ml",
+    price: "$000",
+  },
+  {
+    name: "Nombre del Mocktail 4",
+    country: "PAIS",
+    detail: "000 ml",
+    price: "$000",
+  },
+];
+
+export { navLinks, cocktailLists, mockTailLists };
